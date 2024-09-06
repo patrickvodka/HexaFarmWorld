afc31da977e15d48ecb271cc4de63a22770c3e41
Patou_ <LeNotraldu22@gmail.com>
2024-09-06 15:01:07 +0200

    spawn editor

--- Assets/Editor/GridWaveFonctListEditor.cs
+++ Assets/Editor/GridWaveFonctListEditor.cs
@@ -8,6 +8,7 @@ public class GridWaveFonctListEditor : Editor
 {
     private GridWaveFonctList _gridWaveFonctList;
     private int[] borders = new int[6];
+    private bool _showDebug = false;
     private void OnEnable()
     {
         _gridWaveFonctList = (GridWaveFonctList)target;
@@ -16,30 +17,75 @@ public class GridWaveFonctListEditor : Editor
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
-        GUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        if (GUILayout.Button("Afficher le Debugger"))
+        {
+            _showDebug = !_showDebug;
+        }
         
-        for (int i = 0; i < 6; i++)
+        if (_showDebug)
         {
-            EditorGUILayout.LabelField($"Border {i}:", EditorStyles.boldLabel, GUILayout.Width(60));
-            borders[i] = EditorGUILayout.IntField(borders[i], GUILayout.Width(25));
-            
-        }
-        GUILayout.EndHorizontal();
-        if (GUILayout.Button("Afficher Dictionnaire des Hashes"))
-        {
-            ShowTileHashes();
-        }
-        if (GUILayout.Button("Afficher Dictionnaire précis de Hashes"))
-        {
-            
-            BigInteger test = _gridWaveFonctList.GetBorderHash(this.borders);
-            ShowLastHashes(test);
-            //int[] borders = _gridWaveFonctList.ReverseHash(test, 6);
-            //Debug.LogError($"Les bordures retrouvées sont : {string.Join(", ", borders)}");
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            EditorGUILayout.LabelField("Debuging Option : ", EditorStyles.boldLabel, GUILayout.MaxHeight(20));
+            EditorGUILayout.Space();
+            GUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+            EditorGUILayout.LabelField("Faces : ", EditorStyles.boldLabel, GUILayout.MaxWidth(60));
+            for (int i = 1; i < 7; i++)
+            {
+                EditorGUILayout.LabelField(" | ", EditorStyles.boldLabel, GUILayout.MaxWidth(10));
+                //EditorGUILayout.LabelField("", EditorStyles.boldLabel, GUILayout.Width(60));
+                 EditorGUILayout.LabelField($"{i}", GUILayout.Width(20));
+                // EditorGUILayout.LabelField(" | ", EditorStyles.boldLabel,GUILayout.MaxWidth(10));
 
+            }
+            EditorGUILayout.LabelField(" | ", EditorStyles.boldLabel, GUILayout.MaxWidth(10));
+            GUILayout.EndHorizontal();
+            // space
+            //EditorGUILayout.Space();
+            //EditorGUILayout.Space();
+
+            GUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+            EditorGUILayout.LabelField("Borders : ", EditorStyles.boldLabel, GUILayout.MaxWidth(60));
+            for (int i = 0; i < 6; i++)
+            {
+                EditorGUILayout.LabelField(" | ", EditorStyles.boldLabel, GUILayout.MaxWidth(10));
+                //EditorGUILayout.LabelField("", EditorStyles.boldLabel, GUILayout.Width(60));
+                borders[i] = EditorGUILayout.IntField(borders[i], GUILayout.Width(20));
+                if(borders[i] > 99 )
+                {
+                    EditorGUILayout.HelpBox("The number is too high! Please enter a value of 99 or less.", MessageType.Warning);
+                }
+                else if( borders[i] < 0)
+                {
+                    EditorGUILayout.HelpBox("The number is too low! Please enter a positive value.", MessageType.Warning);
+                }
+                // EditorGUILayout.LabelField(" | ", EditorStyles.boldLabel,GUILayout.MaxWidth(10));
+
+            }
+            EditorGUILayout.LabelField(" | ", EditorStyles.boldLabel, GUILayout.MaxWidth(10));
+            
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+            if (GUILayout.Button("Afficher Dictionnaire des Hashes"))
+            {
+                ShowTileHashes();
+            }
+            EditorGUILayout.Space();
+            if (GUILayout.Button("Afficher Dictionnaire précis de Hashes"))
+            {
+
+                BigInteger test = _gridWaveFonctList.GetBorderHash(this.borders);
+                ShowLastHashes(test);
+                //int[] borders = _gridWaveFonctList.ReverseHash(test, 6);
+                //Debug.LogError($"Les bordures retrouvées sont : {string.Join(", ", borders)}");
+
+            }
         }
+
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
     }
 
+   
     private void ShowLastHashes(BigInteger currentHash)
     {
         var tileHashes = _gridWaveFonctList.hashToTileMap;
@@ -105,4 +151,5 @@ public class GridWaveFonctListEditor : Editor
             }
         }
     }
+
 }
